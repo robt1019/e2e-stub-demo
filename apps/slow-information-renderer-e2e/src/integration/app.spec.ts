d1989f43dc0bfe31a0eee75db0e77ce44c23cd27
robt1019 <robert.i.taylor88@gmail.com>
2019-04-23 22:25:42 +0100

    fix(): add a wait to e2e test. Looks good

--- apps/slow-information-renderer-e2e/src/integration/app.spec.ts
+++ apps/slow-information-renderer-e2e/src/integration/app.spec.ts
@@ -3,6 +3,7 @@ describe('slow-information-renderer', () => {
 
   it('should display an interesting fact when the button is clicked', () => {
     cy.get('#interestingFactButton').click();
+    cy.wait(10000);
     cy.get('#interestingFact');
   });
 });
