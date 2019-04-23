f3f762dd0563bd652bc1a5f50ca23e63be859599
robt1019 <robert.i.taylor88@gmail.com>
2019-04-23 23:51:29 +0100

    test(slow-information-renderer): control stub via window

--- apps/slow-information-renderer-e2e/src/integration/app.spec.ts
+++ apps/slow-information-renderer-e2e/src/integration/app.spec.ts
@@ -3,11 +3,20 @@ describe('slow-information-renderer', () => {
 
   it('should display an interesting fact when the button is clicked', () => {
     cy.get('#interestingFactButton').click();
-    cy.wait(10000);
     cy.get('#interestingFact');
   });
 
   it('should show a show more info button if the fact is especially interesting to give user more info', () => {
+    /**
+     * Tell our stub at run time to return the most interesting fact
+     */
+    cy.window().then(win => {
+      win['stubSlowInfoService'].returnInterestingFact();
+    });
+
+    cy.get('#interestingFactButton').click();
+    cy.get('#interestingFact');
+
     cy.get('#showMoreInfoButton').click();
     cy.get('#furtherInformation');
   });
--- apps/slow-information-renderer/src/app/stub-slow-information.service.ts
+++ apps/slow-information-renderer/src/app/stub-slow-information.service.ts
@@ -6,13 +6,28 @@ import { SlowInformation } from '@wtfisanapi/slow-ass-api';
   providedIn: 'root'
 })
 export class StubSlowInformationService implements SlowInformationService {
-  getSlowInfo() {
-    return Promise.resolve({
-      topic: 'mammals',
-      fact: {
-        interestLevel: 'holyCrudThatIsInteresting',
-        body: 'have a tendency to skirt the issue'
+  private testSlowData: SlowInformation = {
+    topic: 'mammals',
+    fact: {
+      interestLevel: 'reallyInteresting',
+      body: 'have a tendency to skirt the issue'
+    }
+  };
+
+  constructor() {
+    window['stubSlowInfoService'] = {
+      returnInterestingFact: () => {
+        this.testSlowData.fact.interestLevel = 'holyCrudThatIsInteresting';
+      },
+      returnLessInterestingFact: () => {
+        this.testSlowData.fact.interestLevel = 'reallyInteresting';
+      },
+      returnLeastInterestingFact: () => {
+        this.testSlowData.fact.interestLevel = 'interesting';
       }
-    } as SlowInformation);
+    };
+  }
+  getSlowInfo() {
+    return Promise.resolve(this.testSlowData as SlowInformation);
   }
 }
